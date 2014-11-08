5595482b25657b404a27db0d45faebf98eb5ffaa
peterfalken <pupusa00>
2014-11-08 15:26:43 -0600

    More controllers Added BootStrap CSS

--- src/main/webapp/resources/js/ngApp_base.js
+++ src/main/webapp/resources/js/ngApp_base.js
@@ -1,8 +1,24 @@
 /**
  * Created by luis on 11/8/14.
  */
-var appUser = {username: 'luis', password: 'password', loggedIn: false};
-var app = angular.module('baseApp',[]);
-app.controller('LoginController', function(){
-    this.user = appUser;
-});
+(function () {
+    var appUser = {username: 'luis', password: 'password', loggedIn: false};
+    var app = angular.module('baseApp', []);
+
+    app.controller('LoginController', function () {
+        this.user = appUser;
+    });
+
+    app.controller('AppUserDirectoryController', function () {
+        this.directoryLetters = "";
+
+        this.appUserForLetter = fucntion(letter){
+            if(letter){
+                //get AppUsers for Letter.
+            }
+        };
+    });
+
+
+
+})();
