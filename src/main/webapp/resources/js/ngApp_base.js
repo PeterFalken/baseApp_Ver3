9693d7cfd8406624638bd752adf2f96af24b427b
peterfalken <pupusa00>
2014-11-08 15:46:43 -0600

    Correction to typos on: - More controllers - Added BootStrap CSS

--- src/main/webapp/resources/js/ngApp_base.js
+++ src/main/webapp/resources/js/ngApp_base.js
@@ -10,15 +10,14 @@
     });
 
     app.controller('AppUserDirectoryController', function () {
-        this.directoryLetters = "";
+        this.directoryLetters = ['A','B','C'];
 
-        this.appUserForLetter = fucntion(letter){
+        this.appUserForLetter = function(letter){
             if(letter){
                 //get AppUsers for Letter.
+                console.log("We have appUsers");
             }
         };
     });
-
-
 
 })();
